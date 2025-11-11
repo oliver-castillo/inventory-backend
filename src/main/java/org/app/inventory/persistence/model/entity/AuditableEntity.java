ec14fc71479000428861cd402edbbf072a0e3844
Oliver Castillo <olcastillot@gmail.com>
2025-11-10 23:51:07 -0500

    feat: add base entity classes with audit functionality

---
+++ src/main/java/org/app/inventory/persistence/model/entity/AuditableEntity.java
@@ -0,0 +1,41 @@
+package org.app.inventory.persistence.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class AuditableEntity extends BaseEntity {
+  @CreatedDate
+  @NotNull
+  @Column(name = "\"CREATED_AT\"", nullable = false)
+  private Instant createdAt;
+
+  @LastModifiedDate
+  @NotNull
+  @Column(name = "\"UPDATED_AT\"", nullable = false)
+  private Instant updatedAt;
+
+  @CreatedBy
+  @Column(name = "\"CREATED_BY\"", nullable = false)
+  private String createdBy;
+
+  @LastModifiedBy
+  @Column(name = "\"UPDATED_BY\"", nullable = false)
+  private String updatedBy;
+}
